6c3006438bee124da713ac8152390592f2f9c4d8
Paul Marganian <pmargani@nrao.edu>
2019-02-08 15:25:37 -0500

    Added code for plotting images of zernikies
    by reading asdata.txt file, not just the FITS

--- analyzeActiveSurface.py
+++ analyzeActiveSurface.py
@@ -1,3 +1,5 @@
+import os
+
 from astropy.io import fits
 try:
     import matplotlib.pylab as plt
@@ -6,6 +8,8 @@ except:
     print "No Plotting for you!"
 
 import numpy as np
+
+from ProjectScanLog import ProjectScanLog
 
 def plotData(data, scanNumber, fieldName):
     fig = plt.figure()
@@ -131,12 +135,129 @@ def diffField(fn1, fn2, tableName, fieldName):
 
     return data1 - data2, data1, data2
 
+def readActSrfTxt(fn):
+
+    f = open(fn, 'r')
+    ls = f.readlines()
+
+    x = []
+    y = []
+    z = []
+
+    # skip the comments in the first two lines:
+    print ls[:2]
+    for l in ls[2:]:
+        ps = l.split(' ')
+        x.append(float(ps[0]))
+        y.append(float(ps[1]))
+        z.append(int(ps[2][:-1]))
+
+    return np.array(x), np.array(y), np.array(z)
+
+def smoothSlow(az, el, r, n, sigEl=None, sigAz=None):
+    "smooth our data"
+
+    azRange = np.linspace(min(az), max(az), n)
+    elRange = np.linspace(min(el), max(el), n)
+
+    azLoc, elLoc = np.meshgrid(azRange, elRange)
+
+    if sigEl is None:
+        sigEl=0.001
+    if sigAz is None:    
+        sigAz=0.001;
+
+    # init our smoothing result
+    rSm = np.ndarray(shape=(n,n))
+    rSms = []
+    for j in range(n):
+        # print "J:", j
+        for k in range(n):
+            w=2*np.pi*np.exp( (- (az - azLoc[j,k])**2 /( 2.*sigAz**2 )-(el-elLoc[j,k])**2 /(2.*sigEl**2 )))
+            norm=sum(w)
+            if norm==0:
+                norm=1
+                rSm[j,k]=np.nan #0 #min( r )
+            else:
+                w = w / norm
+                rSm[j,k] = sum(r * w)
+
+    return (azLoc, elLoc, rSm)
+
+def plotActSrfTxt(fn):
+
+    x, y, z = readActSrfTxt(fn)
+
+    f = plt.figure()
+    ax = Axes3D(f)
+    ax.scatter(x, y, z)
+    plt.title("Act Srf Txt")
+
+    # now regrid the data
+    N = 100
+    sigX = sigY = .1
+    xLoc, yLoc, zSmooth = smoothSlow(x, y, z, N, sigEl=sigX, sigAz=sigY)
+
+    print "Smoothed data using %s x %d size grid, sigs: %f, %f" % (N, N, sigX, sigY)
+
+    f = plt.figure()
+    ax = Axes3D(f)
+    ax.plot_surface(xLoc, yLoc, zSmooth)
+    plt.title("Act Srf Txt Smoothed")
+
+    f = plt.figure()
+    ax = f.gca()
+    cax = ax.imshow(zSmooth)
+    cbar = f.colorbar(cax)
+    plt.title("Act Srf Txt Smoothed")
+    plt.show()
+
+def analyzeActiveSurfaceScan(path, fn, scanNum):
+
+    print "Scan: ", scanNum
+    fitsPath = os.path.join(path, fn)
+
+    print "FITS: ", fitsPath
+    plotFile(fitsPath)
+
+    txtFile = "asdata.%s.txt" % scanNum
+    txtPath = os.path.join(path, txtFile)
+
+    print "Txt:", txtPath
+    if os.path.isfile(txtPath):    
+        plotActSrfTxt(txtPath)
+    else:
+        print "no asdata.*.txt: ", txtPath
+
+def analyzeActiveSurfaceScans(scanLogPath, scanNums):
+
+    device = "ActiveSurfaceMgr"
+    p = ProjectScanLog(scanLogPath)
+    p.open()
+    
+    for scanNum in scanNums:
+        print ""
+        f = p.getDeviceFilename(device, scanNum)
+        path = os.path.join(scanLogPath, device)
+        analyzeActiveSurfaceScan(path, f, scanNum)
+
+def parseAsZernikeConf(fn):
+
+    f = open(fn, 'r')
+
+    # skipp comments
+    ls = f.readlines()
+    for l in ls[7:]:
+        ps = l.split(' ')
+        # actuator[rib][hoop] x y rho theta phi rho_y theta_y phi_y
 
 def main():
-    fn  = 'data/ActiveSurfaceMgr/2019_02_07_17:33:29.fits'
-    fn2 = 'data/ActiveSurfaceMgr/2019_02_07_18:38:01.fits'
+    #fn  = 'data/ActiveSurfaceMgr/2019_02_07_17:33:29.fits'
+    #fn2 = 'data/ActiveSurfaceMgr/2019_02_07_18:38:01.fits'
     #plotFile(fn)
-    print diffField(fn, fn2, "SURFACE", "ABSOLUTE")
+    #print diffField(fn, fn2, "SURFACE", "ABSOLUTE")
+    path = "/users/pmargani/tmp/simdata/TINT_080219/"
+    analyzeActiveSurfaceScans(path, [1, 2])
 
 if __name__ == '__main__':
     main()
