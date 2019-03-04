26c37852a6fddfc4c160ba97dda82ac4e786131c
Paul Marganian <pmargani@nrao.edu>
2019-03-04 15:10:15 -0500

    processPTX now seems to work (with unit tests)

---
+++ TestProcessPTX.py
@@ -0,0 +1,247 @@
+import os
+import csv
+import unittest
+from copy import copy
+
+import numpy as np
+
+#from parabolas import *
+from processPTX import * 
+
+class TestProcessPTX(unittest.TestCase):
+
+    def setUp(self):
+
+        # base case 
+        f = 5.0
+        v1x = 0.0
+        v1y = 0.0
+        v2 = 10.0
+        xRot = np.pi/2
+        yRot = 0. 
+        self.data = [f, v1x, v1y, v2, xRot, yRot]
+
+        # base tolerance
+        self.tol = 1e-3
+        self.places = 3
+
+    def almostEqualMatrix(self, exp, actual, places=None):
+
+        if places is None:
+            places = self.places
+
+        # TBF: better way to do this?
+        self.assertEqual(actual.shape, exp.shape)
+
+        w, h = exp.shape
+        for i in range(w):
+            for j in range(h):
+                # TBF: why does this not work?
+                #print i, j, exp[i][j], actual[i][j], type(exp[i][j]), type(actual[i][j])
+                #self.assertAlmostEquals(exp[i][j], actual[i][j], tol)
+                second = exp[i][j]
+                first = actual[i][j]
+                self.assertTrue(round(abs(second-first), places) == 0)
+
+    def testRotateXYaboutZ_0degrees(self):
+        "data not rotated by roateXYaboutZ is equal to itself"
+
+        # data in the xyz direction
+        xyz = np.array([(i, 0, 0) for i in range(10)])
+
+        # identity test
+        newXyz = rotateXYaboutZ(xyz, 0.)
+
+        self.almostEqualMatrix(xyz, newXyz)
+
+    def testRotateXYaboutZ_90degrees(self):
+        "x data rotated 90 degrees by roateXYaboutZ becomes negative y data"
+
+        # data in the xyz direction
+        xyz = np.array([(i, 0, 0) for i in range(10)])
+
+        newXyz = rotateXYaboutZ(xyz, 90.)
+
+        exp = np.array([(0., float(i), 0.) for i in range(10)])
+
+        self.almostEqualMatrix(exp, newXyz)
+
+    def testRotateXYaboutZ_180degrees(self):
+        "x data rotated 180 degrees by roateXYaboutZ becomes negative x data"
+
+        # data in the xyz direction
+        xyz = np.array([(i, 0, 0) for i in range(10)])
+
+        newXyz = rotateXYaboutZ(xyz, 180.)
+
+        exp = np.array([(-float(i), 0., 0.) for i in range(10)])
+
+        self.almostEqualMatrix(exp, newXyz)
+
+    def testFitlerOutRadius(self):
+
+        xyz = [
+            (0,0,0),
+            (1,1,0),
+            (1,2,0),
+            (2,2,0)
+        ]
+
+        # make the radius big enough to take in everything
+        r = filterOutRadius(xyz, searchRadius=4, mysteryX=0)
+        self.assertTrue((r == xyz).all()) 
+
+        # now shrink the radius
+        exp = xyz[:2]
+        r = filterOutRadius(xyz, searchRadius=2, mysteryX=0)
+        self.assertTrue((r == exp).all()) 
+
+        # increase radius, but add extension in x
+        exp = np.array(xyz[:3])
+        r = filterOutRadius(xyz, searchRadius=4, mysteryX=2)
+        self.assertTrue((r == exp).all()) 
+
+    def testProcessPTXdata(self):
+
+        hdr = ['\n']*10
+        ls = [
+            '0 0 0 0.500000\n',
+            '0 0 0 0.500000\n',
+            '26.277939 -18.431900 -44.699600 0.031861\n',
+            '26.302536 -18.449173 -44.682480 0.252079\n',
+        ]
+
+        # don't rotate the data, and don't filter by radius
+        rs = processPTXdata(hdr + ls, 0., 1e10)
+
+        # exp answer means the data doesn't change
+        exp = np.array([
+            [26.277939, -18.4319,   -44.6996],
+            [26.302536, -18.449173, -44.68248]
+        ])
+
+        self.almostEqualMatrix(exp, rs)
+
+    def testProcessPTXdata2(self):
+
+        hdr = ['\n']*10
+        ls = [
+            '0 0 0 0.500000\n',
+            '0 0 0 0.500000\n',
+            '26.277939 -18.431900 -44.699600 0.031861\n',
+            '26.302536 -18.449173 -44.682480 0.252079\n',
+        ]
+
+        # process with values we've seen in the wild 
+        rs = processPTXdata(hdr + ls, 150., 1e10) #47)
+
+        # exp answer 
+        exp = np.array([
+            [-13.54141273, 29.10146314, -44.6996],
+            [-13.55407786, 29.1287205, -44.68248]
+        ])
+
+        self.almostEqualMatrix(exp, rs)
+        
+        # we see that when we filter by the usual radius, we filter out all our test data
+        rs = processPTXdata(hdr + ls, 150., 47)
+
+        self.assertEqual(len(rs), 0)
+
+        # now add some data that won't get filtered out
+        line = '54 0 -44.682480 0.252079\n'
+        newLs = hdr + ls + [line]
+
+        rs = processPTXdata(newLs, 150., 47)
+
+        self.assertEqual(len(rs), 1)
+        exp = np.array([-46.76537180435968,27.,-44.68248])
+        exp.shape = (1,3)
+        self.almostEqualMatrix(exp, rs)
+ 
+    def testProcessPTX2(self):
+
+        fn = "Test1_STA14_Bump1_High-02_METERS_SAMPLE.ptx"
+        fpath = os.path.join("data", fn)
+
+        # make sure input exists
+        self.assertTrue(os.path.isfile(fpath))
+
+        opath = fpath + ".csv"
+
+        # make sure the expected output does NOT exist yet
+        if os.path.isfile(opath):
+            os.remove(opath)
+        self.assertTrue(not os.path.isfile(opath))
+
+        # now create the file we just might have cleaned up
+        processPTX(fpath, rotationAboutZdegrees=150., searchRadius=47)
+
+        # make sure output exists
+        self.assertTrue(os.path.isfile(opath))
+
+        # now parse it
+        xyzs = []
+        fieldnames = ['x', 'y', 'z']
+        with open(opath, 'r') as f:
+            reader = csv.DictReader(f, fieldnames=fieldnames)
+            for row in reader:
+                 xyz = (float(row['x']),
+                 float(row['y']),
+                 float(row['z']))
+                 xyzs.append(xyz)
+        
+        exp = [
+            (-46.76537180435968, 27., -44.68248)
+        ]
+
+        self.almostEqualMatrix(np.array(xyzs), np.array(exp))
+
+
+    def testProcessPTX(self):
+
+        fn = "Test1_STA14_Bump1_High-02_METERS_SAMPLE.ptx"
+        fpath = os.path.join("data", fn)
+
+        # make sure input exists
+        self.assertTrue(os.path.isfile(fpath))
+
+        opath = fpath + ".csv"
+
+        # make sure the expected output does NOT exist yet
+        if os.path.isfile(opath):
+            os.remove(opath)
+        self.assertTrue(not os.path.isfile(opath))
+
+        # now create the file we just might have cleaned up
+        processPTX(fpath, rotationAboutZdegrees=0, searchRadius=1e10)
+
+        # make sure output exists
+        self.assertTrue(os.path.isfile(opath))
+
+        # now parse it
+        xyzs = []
+        fieldnames = ['x', 'y', 'z']
+        with open(opath, 'r') as f:
+            reader = csv.DictReader(f, fieldnames=fieldnames)
+            for row in reader:
+                 xyz = (float(row['x']),
+                 float(row['y']),
+                 float(row['z']))
+                 xyzs.append(xyz)
+        
+        exp = [
+            (26.456772, -18.582199, -44.610275),
+            (26.486862, -18.603317, -44.60231),
+            (26.519547, -18.626236, -44.598648),
+            (26.54744, -18.645828, -44.586563),
+            (26.573868, -18.664383, -44.572403),
+            (26.277939, -18.4319, -44.6996),
+            (26.302536, -18.449173, -44.68248),
+            (54.0, 0.0, -44.68248)
+        ]
+
+        self.almostEqualMatrix(np.array(xyzs), np.array(exp))
+
+
+
--- processPTX.py
+++ processPTX.py
@@ -1,19 +1,69 @@
 import sys
 
 import numpy as np
+from astropy import units as u
+from astropy.coordinates import CartesianRepresentation
+from astropy.coordinates.matrix_utilities import rotation_matrix
 
-def processPTX(fpath):
+def rotateXYaboutZ(xyz, rotDegrees):
 
+    # define it as cartesian
+    rep = CartesianRepresentation(xyz[:, 0], xyz[:, 1], xyz[:, 2])
+
+    # define our rotation about z
+    # TBF: why must we negate this to match Mathematica results?
+    rot = rotation_matrix(-rotDegrees * u.deg, axis='z')
+
+    # apply the rotation
+    new_rep = rep.transform(rot)
+
+    new_xyz = new_rep.get_xyz().value
+
+    # get it back into original form
+    # TBF: must be a faster method!
+    xyzNew = []
+    x = new_xyz[0]
+    y = new_xyz[1]
+    z = new_xyz[2]
+    for i in range(len(x)):
+        xyzNew.append((x[i], y[i], z[i]))
+        
+    return np.array(xyzNew)    
+        
+    
+def processPTX(fpath, rotationAboutZdegrees=None, searchRadius=None):
+
+ 
+    if rotationAboutZdegrees is None:
+        rotationAboutZdegrees = 150.
+    if searchRadius is None:    
+        searchRadius = 47
+   
     with open(fpath, 'r') as f:
         ls = f.readlines()
+    
+    xyz = processPTXdata(ls, rotationAboutZdegrees, searchRadius)
 
-    print "Original file num lines: ", len(ls)
+    # write to CSV file
+    outf = fpath + ".csv"
+    np.savetxt(outf, xyz, delimiter=",")
 
-    badLine = '0 0 0 0.500000\r\n'
+def processPTXdata(lines, rotationAboutZdegrees, searchRadius, quiet=True):
+
+    ls = lines
+
+    if not quiet:
+        print "Original file num lines: ", len(ls)
+
+    #badLine = '0 0 0 0.500000\r\n'
+    badLine = '0 0 0 0.500000\n'
     # reject the '0 0 0 0.500000' lines - TBF: what are those?
     ls = [l for l in ls if l != badLine]
+    #for l in ls:
+    #    print "'%s' vs '%s'" % (l, badLine)
 
-    print "File minus '0 0 0 0.50000' lines: ", len(ls)
+    if not quiet:
+        print "File minus '0 0 0 0.50000' lines: ", len(ls)
 
     #print "beginning of file: "
     #print ls[:12]
@@ -38,6 +88,7 @@ def processPTX(fpath):
     # rotation!  This takes a long time.  TBF: why? *)
     #rot=AffineTransform[{RotationMatrix[150Degree,{0,0,1}],{0,0,0}}];
     #lall=Map[rot,lall];
+    xyz = rotateXYaboutZ(xyz, rotationAboutZdegrees)
 
     # and we only use those parts that are within this radius TBF? *)
     # ls=Select[ls,Norm[Take[#,2]-{-54,0}]<47&]
@@ -47,21 +98,34 @@ def processPTX(fpath):
     # What's happening there is that the x, y from each element in xyz above is taken,
     # and 54 is added to each x.  Norm == Sqrt(x**2 + y**2); that looks like a radius to me.
     # so, if the radius is less the 47, this data element is kept.
-    mysteryX = 54
-    mysteryRadius = 47
+
+    return filterOutRadius(xyz, searchRadius=searchRadius)
+
+def filterOutRadius(xyz, searchRadius=None, mysteryX=None):
+    "return only those xyz points where sqrt(x**2 + y**2) is within a limit"
+
+    if mysteryX is None:
+        mysteryX = 54
+    if searchRadius is None:    
+        searchRadius = 47
+
     xyzInside = []
     for x, y, z in xyz:
-        x += mysteryX
-        r = np.sqrt(x**2 + y**2)
-        if r < mysteryRadius:
+        x2 = x + mysteryX
+        r = np.sqrt(x2**2 + y**2)
+        if r < searchRadius:
             xyzInside.append((x, y, z))
     xyzInside = np.array(xyzInside)
 
-    # write to CSV file
-    outf = fpath + ".csv"
-    numpy.savetxt(outf, xyzInside, delimiter=",")
+    return xyzInside
+
     
 
 if __name__ == "__main__":
-    fpath = sys.argv[1]
+
+    #fpath = "/home/sandboxes/pmargani/LASSI/data/LeicaDropbox/PTX/Test1_STA14_Bump1_High-02_METERS.ptx"
+    #fpath = sys.argv[1]
+    #testRotation(90.)
+    fpath = "data/Test1_STA14_Bump1_Hig-02_METERS_SAMPLE.ptx"
     processPTX(fpath)
+
