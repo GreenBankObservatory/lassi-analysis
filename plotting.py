5dd34b9636f659c1cafd1dfcdf16825064443093
Paul Marganian <pmargani@nrao.edu>
2019-06-14 10:53:10 -0400

    copied plotting from parabolas to this module

---
+++ plotting.py
@@ -0,0 +1,69 @@
+from copy import copy
+import random
+
+import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import axes3d, Axes3D
+
+def imagePlot(z, title):
+    fig = plt.figure()
+    ax = fig.gca()
+    #cax = ax.imshow(np.log(np.abs(np.diff(zrr - newZ))))
+    cax = ax.imshow(z)
+    fig.colorbar(cax)
+    plt.title(title)
+
+def surface3dPlot(x, y, z, title, xlim=None, ylim=None, sample=None):
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    # plot all the data, or just some?
+    if sample is not None:
+        print "Plotting %5.2f percent of data" % sample
+        x, y, z = sampleXYZData(x, y, z, sample)
+
+    ax.plot_surface(x, y, z)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(title)
+    if xlim is not None:
+        plt.xlim(xlim)
+    if ylim is not None:
+        plt.ylim(ylim)
+
+def scatter3dPlot(x, y, z, title, xlim=None, ylim=None, sample=None):
+
+    # plot all the data, or just some?
+    if sample is not None:
+        print "Plotting %5.2f percent of data" % sample
+        x, y, z = sampleXYZData(x, y, z, sample)
+        print "Now length of data is %d" % len(x)
+
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.scatter(x, y, z)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(title)
+    if xlim is not None:
+        plt.xlim(xlim)
+    if ylim is not None:
+        plt.ylim(ylim)    
+
+
+def sampleXYZData(x, y, z, samplePercentage, seed=None):
+    "Return a random percentage of the data"
+
+    assert len(x) == len(y)
+    assert len(y) == len(z)
+
+    if seed is not None:
+        random.seed(seed)
+
+    lenx = len(x)
+
+    sampleSize = int((lenx * samplePercentage) / 100.)
+
+    idx = random.sample(range(lenx), sampleSize)
+
+    return copy(x[idx]), copy(y[idx]), copy(z[idx])
