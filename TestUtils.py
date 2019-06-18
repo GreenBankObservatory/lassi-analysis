3da88f0f6e3d7c8c658b59d44a66059bcbc0a670
Paul Marganian <pmargani@nrao.edu>
2019-06-18 16:59:35 -0400

    Finally added utils module
    for stuff like coord transforms etc.
    TODO: cleanup funcs in other modules that should be in utils

---
+++ TestUtils.py
@@ -0,0 +1,35 @@
+"Unit tests for Utils module"
+
+import unittest
+
+import numpy as np
+
+import matplotlib
+matplotlib.use('agg')
+
+from utils import sph2cart, cart2sph
+
+class TestUtils(unittest.TestCase):
+    "unit tests for Utils module"
+
+    # def setUp(self):
+
+    def testCoordinateIdentity(self):
+        "test sph2cart -> cart2sph -> original values"
+
+        # create some test data
+        xs = np.array(range(10))
+        ys = np.array(range(10)) * -1.0
+        zs = (xs**2 + ys**2) + 10.
+
+        # transform it - there and back again
+        # el == latitude
+        # az == longitude
+        r, el, az = cart2sph(xs, ys, zs, verbose=False)
+        x2, y2, z2 = sph2cart(el, az, r, verbose=False)
+
+        tol = 1e-6
+
+        self.assertTrue(np.all(np.abs(x2-xs) < tol))
+        self.assertTrue(np.all(np.abs(y2-ys) < tol))
+        self.assertTrue(np.all(np.abs(z2-zs) < tol))
---
+++ utils.py
@@ -0,0 +1,62 @@
+
+import numpy as np
+from astropy.coordinates import cartesian_to_spherical
+from astropy.coordinates import spherical_to_cartesian
+
+def cart2sph(x, y, z, verbose=True):
+    "Wrapper around astropy's cartesian_to_spherical"
+    rs, lats, lngs = cartesian_to_spherical(x, y, z)
+
+    # convert from astropy Quantities to simple numpy
+    rs = np.array([r.value for r in rs])
+    lats = np.array([l.value for l in lats])
+    lngs = np.array([l.value for l in lngs])
+
+    if verbose:
+        print "min/max lats (radians)", lats.min(), lats.max()
+        print "lats supposed range (radians)", -np.pi/2, np.pi/2
+        print "min/max lngs (radians)", lngs.min(), lngs.max()
+        print "lngs supposed range (radians)", 0, 2*np.pi
+
+    return rs, lats, lngs
+
+def sph2cart(az, el, r, verbose=True):
+    "Wrapper around astropy's spherical_to_cartesian"
+
+    if verbose:
+        print "min/max az (radians)", np.nanmin(az), np.nanmax(az)
+        print "az supposed range (radians)", -np.pi/2, np.pi/2
+        print "min/max el (radians)", np.nanmin(el), np.nanmax(el)
+        print "el supposed range (radians)", 0, 2*np.pi
+        print "radial ranges", np.nanmin(r), np.nanmax(r)
+
+    xs, ys, zs = spherical_to_cartesian(r, az, el)
+
+    # convert from astropy Quantities to simple numpy
+    xs = np.array([x.value for x in xs])
+    ys = np.array([y.value for y in ys])
+    zs = np.array([z.value for z in zs])
+
+    return xs, ys, zs 
+
+def aggregateXYZ(x, y, z):
+    "x, y, z -> [(x0, y0, z0), (x1, y1, z1), ...]"
+    xyz = []
+    for i in range(len(x)):
+        xyz.append((x[i], y[i], z[i]))
+    return np.array(xyz)
+
+def splitXYZ(xyz):
+    "[(x0, y0, z0), (x1, y1, z1), ...] -> x, y, z"
+
+    x = []
+    y = []
+    z = []
+
+    for xi, yi, zi in xyz:
+        x.append(xi); y.append(yi); z.append(zi)
+        
+    x = np.array(x)
+    z = np.array(z) 
+    y = np.array(y)    
+    return x, y, z
