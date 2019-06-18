3b778b5a518931ee0b8b04932e00d9eba4f69cd7
Paul Marganian <pmargani@nrao.edu>
2019-06-18 16:57:41 -0400

    lassiAnalysis correclty computes weights using weightSmooth
    TODO: remove failed first attempt at doing this in lassiAnalysis

--- lassiAnalysis.py
+++ lassiAnalysis.py
@@ -5,6 +5,7 @@ analyzing a single scan of Leica data once it's available.
 import os
 import time
 from copy import copy
+from shutil import copyfile
 
 # import matplotlib
 # matplotlib.use('agg')
@@ -12,13 +13,17 @@ import numpy as np
 import opticspy
 
 from processPTX import processPTX, processNewPTX, aggregateXYZ
-from main import smoothGPUs, smoothXYZGpu, splitXYZ, cart2sph, sph2cart
+from main import smoothGPUs, smoothXYZGpu, splitXYZ
 from main import loadLeicaDataFromGpus
 from parabolas import fitLeicaScan, imagePlot, surface3dPlot, radialReplace
+#from parabolas import loadLeicaDataFromGpus
 from zernikeIndexing import noll2asAnsi, printZs
 from simulateSignal import addCenterBump, zernikeFour
 from simulateSignal import zernikeFive, gaussian
-from plotting import sampleXYZData
+from plotting import sampleXYZData, scatter3dPlot
+from utils import sph2cart, cart2sph
+
+from weightSmooth import weightSmooth
 
 # where is the code we'll be running?
 GPU_PATH = "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing"
@@ -50,13 +55,18 @@ def smooth(fpath, N=512, spherical=False):
 
     return outfiles
 
-def writeGPUoutput(gpuPath, fn, dim, data):
+def writeGPUoutput(gpuPath, fn, dim, data, cp2ext=None):
 
     fn = "%s.%s.csv" % (fn, dim)
     fpath = os.path.join(gpuPath, fn)
     # here we make sure each element gets its own line
     # with as much precision as what I think the GPU is doing
     print "writeGPUoutput to ", fpath
+    if cp2ext is not None and os.path.isfile(fpath):
+        # avoid overwriting this file by copying to a location with 
+        # this extension
+        copyfile(fpath, fpath + '.' + cp2ext)
+
     np.savetxt(fpath, data, fmt="%.18f")
     return fpath
 
@@ -73,12 +83,27 @@ def smoothWithWeights(fpath, xyz, N=512):
     # GPUs do the coordinate transforms
 
     # convert to shperical
-    print "Converting to spherical ..."
+    # s = 1.0
+    # print "working with %f percent of data" % s
     x, y, z = splitXYZ(xyz)
-    s = 1.0
-    print "working with %f percent of data" % s
-    x, y, z = sampleXYZData(x, y, z, s)
+    # x, y, z = sampleXYZData(x, y, z, s)
+
+    scatter3dPlot(x, y, z, "org sample of data")
+
+    xOrg = copy(x)
+    yOrg = copy(y)
+    zOrg = copy(z)
+
+    print "Converting to spherical ..."
     r, el, az = cart2sph(x, y, z)
+
+    scatter3dPlot(el, az, r, "el az r of sample of data")
+
+    print "input data ranges: "
+    print "r: ", np.nanmin(r), np.nanmax(r), len(r)
+    print "el: ", np.nanmin(el), np.nanmax(el), len(el)
+    print "az: ", np.nanmin(az), np.nanmax(az), len(az)
+
     sph = aggregateXYZ(r, az, el)
 
     # write this to the file format expected by the GPUs,
@@ -90,20 +115,69 @@ def smoothWithWeights(fpath, xyz, N=512):
 
     # TBF: GPU code is still labeling the output files
     # as [x,y,z] even though we are in spherical
+    print "Smoothing R ..."
     smoothSphFiles = smooth(outf, spherical=True)
 
+    # print "Identity Test!"
+    # basename = os.path.basename(outf)
+    # for data, dim in [(r, 'x'), (az, 'y'), (el, 'z')]:
+    #     # write them to files as if the GPUs created them:
+    #     fn = writeGPUoutput(GPU_PATH, basename, dim, data)
+
+    # smoothSphFiles = [
+    #     "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing/Clean9.ptx.sph.csv.x.csv",
+    #     "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing/Clean9.ptx.sph.csv.y.csv",
+    #     "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing/Clean9.ptx.sph.csv.z.csv"    
+    # ]
+ 
+    basename = os.path.basename(outf)
+    gpuSphPath = os.path.join(GPU_PATH, basename)
+    print "loading GPU data from", gpuSphPath
+
     # retrieve the radial values for the variance calculation
-    rs, azs, els = loadLeicaDataFromGpus(fpath)
+    rs, azs, els = loadLeicaDataFromGpus(gpuSphPath)
+
+    scatter3dPlot(els, azs, rs, "az el r of smoothed data")
+
+    # print "rs vs r: ", rs.shape, r.shape, rs, r
+    # assert np.all(rs == r)
+    # assert np.all(azs == az)
+    # assert np.all(els == el)
+
+    # print "smoothed data ranges: "
+    # print "r: ", np.nanmin(r), np.nanmax(r)
+    # print "el: ", np.nanmin(el), np.nanmax(el)
+    # print "az: ", np.nanmin(az), np.nanmax(az)
 
     # convert these back to cartesian, since this is what
     # we'll need for the subsequent fittings
+    print "converting this spherical data to xyz ..."
     xs, ys, zs = sph2cart(els, azs, rs)
-    smoothedFiles = []
-    for data, dim in [(xs, 'x'), (ys, 'y'), (zs, 'z')]:
-        fn = writeGPUoutput(GPU_PATH, data, dim)
-        smoothedFiles.append(fn)
 
-    # write them to files as if the GPUs created them:
+    scatter3dPlot(xs, ys, zs, "smoothed xyz data")
+
+    smoothedFiles = []
+    for data, dim, ext in [(xs, 'x', 'r'), (ys, 'y', 'az'), (zs, 'z', 'el')]:
+        # write them to files as if the GPUs created them:
+        fn = writeGPUoutput(GPU_PATH, basename, dim, data, cp2ext=ext)
+        smoothedFiles.append(fn)
+    print "cartesian smoothed files: ", smoothedFiles    
+
+    # print "Identity Test Again!"
+    # print "xs vs xOrg", xs.shape, xOrg.shape, xs, xOrg
+
+    # tol = 1e-3
+    # assert np.all(np.abs((xs - xOrg)) < tol)
+    # assert np.all(np.abs((ys - yOrg)) < tol)
+    # assert np.all(np.abs((zs - zOrg)) < tol)
+    # assert np.all(xs == xOrg)
+    # assert np.all(ys == yOrg)
+    # assert np.all(zs == zOrg)
+
+    # fake the weights
+    # weights = np.zeros((N, N))
+    # weights = weights + 1
+    # return smoothedFiles, weights
 
     # now we need to smooth again, but this time using
     # our radial values SQUARED.  So write this to file
@@ -111,14 +185,30 @@ def smoothWithWeights(fpath, xyz, N=512):
     outf2 = fpath + ".sph2.csv"
     np.savetxt(outf2, sph2, delimiter=",")    
 
+    scatter3dPlot(el, az, r**2, "az el r^2")
+
+    print "Smoothing R^2 ..."
     smoothSphFiles2 = smooth(outf2, spherical=True)
+    # smoothSphFiles = [
+    #     "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing/Clean9.ptx.sph2.csv.x.csv",
+    #     "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing/Clean9.ptx.sph2.csv.y.csv",
+    #     "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing/Clean9.ptx.sph2.csv.z.csv"    
+    # ]
 
     # retrieve the radial values for the variance calculation
-    r2s, azs, els = loadLeicaDataFromGpus(fpath)
+    basename2 = os.path.basename(outf2)
+    gpuSphPath2 = os.path.join(GPU_PATH, basename2)
+    print "loading GPU data from", gpuSphPath2
+
+    r2s, azs, els = loadLeicaDataFromGpus(gpuSphPath2)
+
+    scatter3dPlot(els, azs, r2s, "az el r2 of smoothed data")
 
     # now we can calculate the variance!
     # make sure small negative numerical errors are dealt with
     sigma2 = np.abs(r2s - (rs**2))
+
+    print "sigma squared: ", sigma2.shape, np.nanmin(sigma2), np.nanmax(sigma2), np.nanmean(sigma2), np.nanstd(sigma2)
 
     # not normalized weights
     Ws_Not_Norm= 1/sigma2
@@ -129,7 +219,15 @@ def smoothWithWeights(fpath, xyz, N=512):
     # normalize weights, making sure Nans in sum are dealt with
     ws = (Ws_Not_Norm) / np.sum(Ws_Not_Norm[np.logical_not(np.isnan(sigma2))])
 
-    return smoothFiles, ws
+    print "Computed weights: ", ws.shape, ws
+
+    sigma2.shape = (N, N)
+    imagePlot(sigma2, "sigma^2")
+    wsc = copy(ws)
+    wsc.shape = (N, N)
+    imagePlot(wsc, "weights")
+
+    return smoothedFiles, ws
 
 def OLDsmoothWithWeights(fpath, N=512):
     """
@@ -156,6 +254,8 @@ def OLDsmoothWithWeights(fpath, N=512):
 def processLeicaScan(fpath,
                      N=512,
                      rot=None,
+                     xOffset=None,
+                     yOffset=None,
                      sampleSize=None,
                      parabolaFit=None,
                      useFittingWeights=False,
@@ -179,19 +279,24 @@ def processLeicaScan(fpath,
 
     # removes headers, does basic rotations, etc.
     print "Processing PTX file ..."
-    # xOffset = -8.
-    xOffset = -6.
-    yOffset = 50.0
+    if xOffset is None:
+        # xOffset = -8.
+        xOffset = -6.
+    if yOffset is None:    
+        yOffset = 50.0
     if rot is None:
         rot = 0.
     processedPath = fpath + ".csv"
-    if useFittingWeights:    
-        # TBF: for now read the data from the CSV file
-        print "Skipping processing and loading previous values"
-        xyz = np.loadtxt(processedPath, delimiter=',')
-    else:
+    # if useFittingWeights:    
+    #     # TBF: for now read the data from the CSV file
+    #     print "Skipping processing and loading previous values"
+    #     xyz = np.loadtxt(processedPath, delimiter=',')
+    # else:
+    if True:
         xyz = processNewPTX(fpath,
                     rot=rot,
+                    xOffset=xOffset,
+                    yOffset=yOffset,
                     rFilter=True,
                     iFilter=False,
                     parabolaFit=parabolaFit,
@@ -205,12 +310,12 @@ def processLeicaScan(fpath,
     s = time.time()
     print "Smoothing data ..."
 
-
     weights = None
     if useFittingWeights:
-        smoothedFiles, weights = smoothWithWeights(processedPath, xyz, N=N)
+        # smoothedFiles, weights = smoothWithWeights(processedPath, xyz, N=N)
+        smoothedFiles, weights = weightSmooth(processedPath, xyz)
     else:  
-        smoothedFiles = smooth(fpath, xyz, N=N)
+        smoothedFiles = smooth(processedPath)
 
     #   fn = smoothedFiles[0]
     #   assert fn[-5:] == 'x.csv'
@@ -228,11 +333,15 @@ def processLeicaScan(fpath,
     fn = smoothedFiles[0]
     assert fn[-5:] == 'x.csv'
     fn = fn[:-6]
+    print "Fitting data found in files:", fn
+    # print "NOT USING WEIGHTS!"
+    wc = copy(weights)
+    # weights = None
     diff, x, y = fitLeicaScan(fn,
                               numpy=False,
                               N=N,
                               rFilter=False,
-                              inSpherical=useFittingWeights,
+                              # inSpherical=useFittingWeights,
                               weights=weights)
 
     e = time.time()
@@ -257,7 +366,7 @@ def processLeicaScan(fpath,
     finalFile = "%s.processed" % fileBasename
     np.savez(finalFile, xs=xs, ys=ys, diffs=diffs)
 
-    return xs, ys, diffs
+    return xs, ys, diffs, wc
 
 def loadProcessedData(filename):
     "Loads the results of processLeicaScan from file"
---
+++ weightSmooth.py
@@ -0,0 +1,126 @@
+import os
+from copy import copy
+
+import numpy as np
+
+from plotting import scatter3dPlot, sampleXYZData, imagePlot
+from utils import cart2sph, sph2cart, splitXYZ, aggregateXYZ
+from main import loadLeicaDataFromGpus, smoothGPUs
+
+# where is the code we'll be running?
+GPU_PATH = "/home/sandboxes/pmargani/LASSI/gpus/versions/gpu_smoothing"
+
+def weightSmooth(fpath, xyz):
+
+    N = 512
+
+    # convert to shperical
+    # s = 1.0
+    # print "working with %f percent of data" % s
+    x, y, z = splitXYZ(xyz)
+    # x, y, z = sampleXYZData(x, y, z, s)
+
+    scatter3dPlot(x, y, z, "org sample of data", sample=1.0)
+
+    xOrg = copy(x)
+    yOrg = copy(y)
+    zOrg = copy(z)
+
+    # sigma2 = <r>**2 - <r**2>
+    ################# FIRST: calculate <r>
+
+    smoothedXYZfiles = smooth(fpath)
+
+    basename = os.path.basename(fpath)
+    gpuPath = os.path.join(GPU_PATH, basename)
+    print "loading GPU data from", gpuPath
+    xs, ys, zs = loadLeicaDataFromGpus(gpuPath)    
+
+    scatter3dPlot(xs, ys, zs, "sample of smoothed data", sample=1.0)
+
+    # we need the smoothed r, <r>, so convert this
+    print "Converting to spherical ..."
+    rs, els, azs = cart2sph(xs, ys, zs)
+
+
+    ################ Second: calcualte <r**2>
+    # convert the original xyz to spherical
+    r, el, az = cart2sph(x, y, z)
+
+    # square the r term, then go back to cartesian
+    r2 = r**2
+    x2, y2, z2 = sph2cart(el, az, r2)
+
+    # write this to a file for smoothing
+    xyz2 = aggregateXYZ(x2, y2, z2)
+    f2 = basename + ".rs.csv"
+    basepath = os.path.dirname(fpath)     
+    fpath2 = os.path.join(basepath, f2)
+    print "Saving r**2 data in xyz to file", fpath2
+    np.savetxt(fpath2, xyz2, delimiter=',')
+
+    # now smooth this data
+    smoothedXYZ2files = smooth(fpath2)
+    
+    # retrieve the xyz data
+    gpuPath2 = os.path.join(GPU_PATH, f2)
+    print "loading GPU data from", gpuPath2
+    xs2, ys2, zs2 = loadLeicaDataFromGpus(gpuPath2)
+
+    # convert back to shperical to get <r**2>
+    rs2, els2, azs2 = cart2sph(xs2, ys2, zs2)
+
+    ####################: Finally: Calculate Variance
+        # make sure small negative numerical errors are dealt with
+    sigma2 = np.abs(rs2 - (rs**2))
+
+    print "sigma squared: ", sigma2.shape, np.nanmin(sigma2), np.nanmax(sigma2), np.nanmean(sigma2), np.nanstd(sigma2)
+
+    # not normalized weights
+    Ws_Not_Norm= 1/sigma2
+
+    # make sure NaNs induced by zeros in sigma2 are dealt with
+    Ws_Not_Norm[sigma2 == 0.0] = 0.0
+
+    # normalize weights, making sure Nans in sum are dealt with
+    ws = (Ws_Not_Norm) / np.sum(Ws_Not_Norm[np.logical_not(np.isnan(sigma2))])
+
+    print "Computed weights: ", ws.shape, ws
+
+    # plot weights
+    sigma2.shape = (N, N)
+    imagePlot(sigma2, "sigma^2")
+    imagePlot(np.log(np.abs(sigma2)), "log sigma^2")
+    wsc = copy(ws)
+    wsc.shape = (N, N)
+    imagePlot(wsc, "weights")
+    imagePlot(np.log(np.abs(wsc)), "log weights")
+
+    return smoothedXYZfiles, ws
+
+def smooth(fpath, N=512, spherical=False):
+
+    assert os.path.isfile(fpath)
+
+    # we need to specify the aboslute path of our inputs
+    abspath = os.path.abspath(fpath)
+
+    # our output will be same file name, but with appendages
+    outfile = os.path.basename(fpath)
+
+    smoothGPUs(GPU_PATH,
+               abspath,
+               outfile,
+               N,
+               spherical=spherical)
+
+    # make sure the output is where it should be
+    outfiles = []
+    for dim in ['x', 'y', 'z']:
+        dimFile = "%s.%s.csv" % (outfile, dim)
+        dimPath = os.path.join(GPU_PATH, dimFile)
+        outfiles.append(dimPath)
+        assert os.path.isfile(dimPath)
+        print "GPUs created file: ", dimPath
+
+    return outfiles
