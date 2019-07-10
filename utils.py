acd1d05f525c623b7743f9d19d2921b18c0dea6d
Pedro Salas <psalas@nrao.edu>
2019-07-10 10:44:32 -0400

    Added circular_mask funtion to utils

--- utils.py
+++ utils.py
@@ -72,3 +72,33 @@ def difflog(x):
 def log10(x):
     "short cup to numpy log10"
     return np.log10(np.abs(x))
+
+def circular_mask(shape, centre, radius, angle_range):
+    """
+    Return a boolean mask for a circular sector. The start/stop angles in  
+    `angle_range` should be given in clockwise order and in degrees.
+    Centre and radius are in pixels.
+    """
+
+    x,y = np.ogrid[:shape[0],:shape[1]]
+    cx,cy = centre
+    tmin,tmax = np.deg2rad(angle_range)
+
+    # ensure stop angle > start angle
+    if tmax < tmin:
+        tmax += 2*np.pi
+
+    # convert cartesian --> polar coordinates
+    r2 = (x-cx)*(x-cx) + (y-cy)*(y-cy)
+    theta = np.arctan2(x-cx, y-cy) - tmin
+
+    # wrap angles between 0 and 2*pi
+    theta %= (2*np.pi)
+
+    # circular mask
+    circmask = r2 <= radius*radius
+
+    # angular mask
+    anglemask = theta <= (tmax-tmin)
+
+    return circmask*anglemask
