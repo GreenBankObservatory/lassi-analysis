0ba86d7b9c2e8bc5e8614d0c8708b9d65fbf3686
Paul Marganian <pmargani@nrao.edu>
2019-09-27 13:40:40 -0400

    windowData.demoDigitizedWindowScan does just that
    Shows that we can use numpy.digitize to organize the data
    such that means of each bin sensibly represent the original
    data of the primary dish.
    This is a precursor to a function that does for spherical smoothing.

--- windowData.py
+++ windowData.py
@@ -1,4 +1,487 @@
+import os
+import time
+from copy import copy
+import random
+
 import numpy as np
+import dask
+
+from dask import config
+from dask.diagnostics import ProgressBar, Profiler, ResourceProfiler, CacheProfiler
+from dask.diagnostics import visualize
+
+from plotting import imagePlot, scatterPlot, sampleXYZData, scatter3dPlot
+from processPTX import getRawXYZ
+from main import importCsv
+
+#config.set(scheduler='distributed')  # overwrite default with multiprocessing scheduler
+#config.set(scheduler='processes')  # overwrite default with multiprocessing scheduler
+
+# N=20, 11 secs
+# N=50, 63 secs
+
+def digitizeWinDask(x, y, z, N):
+
+    print "len data: ", len(x), len(y), len(z)
+
+    assert len(x) == len(y)
+    assert len(y) == len(z)
+
+    # figure out how to group our data
+    xMin = np.min(x)
+    xMax = np.max(x)
+    yMin = np.min(y)
+    yMax = np.max(y)
+
+    # what's the size of each cell?
+    xBins = np.linspace(xMin, xMax, N)
+    yBins = np.linspace(yMin, yMax, N)
+
+    xd = np.digitize(x, xBins)
+    yd = np.digitize(y, yBins)
+
+    lbins = np.zeros((N, N))
+    total = 0
+    bins = {}
+
+    start = time.time()
+    print "delayed stage ..."
+    for xi in range(1, N+1):
+        print xi
+        for yi in range(1, N+1):
+            t = dask.delayed(getNumInBins(xi, yi, xd, yd))
+            #t = getNumInBins(xi, yi, copy(xd), copy(yd))
+            # t = getNumInBins(xi, yi, xd, yd)
+            #print t
+            #xx = xd == xi
+            #yy = yd == yi
+            #t = len(xd[np.logical_and(xx, yy)])
+            #lbins[xi-1][yi-1] = t
+            bins[(xi-1, yi-1)] = t
+            #total += t
+            
+    end = time.time()
+    print "elapsed secs: ", end - start
+
+    return xd, yd, bins, lbins
+
+    start = time.time()
+    print "persist stage ..."        
+    dask.persist(*bins)
+    end = time.time()
+    print "elapsed secs: ", end - start
+
+    start = time.time()
+    print "compute stage ..."
+    total = 0
+    i = 0
+
+    keys = sorted(bins.keys())
+    for k in keys:
+    # for k, v in bins.items():
+        v = bins[k] 
+        #print i
+        i += 1
+        xi, yi = k
+        #tt = v.compute(scheduler="processes")
+        tt = v.compute()
+        lbins[xi][yi] = tt
+        total += tt
+
+        #rSms = rSms.compute(scheduler="processes")
+
+    end = time.time()
+    print "elapsed secs: ", end - start
+    print "total: ", total
+    #print bins
+    #print lbins
+
+    return xBins, yBins, bins, lbins
+
+def getNumInBins(xi, yi, xd, yd):
+    xx = xd == xi
+    yy = yd == yi
+    return len(xd[np.logical_and(xx, yy)])
+
+def demoDigitize():
+
+    N = 5
+
+    x = np.array(range(10)) 
+    y = np.array(range(10)) 
+
+    # figure out how to group our data
+    xMin = np.min(x)
+    xMax = np.max(x)
+    yMin = np.min(y)
+    yMax = np.max(y)
+
+    # what's the size of each cell?
+    xBins = np.linspace(xMin, xMax, N)
+    yBins = np.linspace(yMin, yMax, N)
+
+    print "x: ", x
+    print "xBins: ", xBins
+
+    xd = np.digitize(x, xBins)
+    print "x digitized: ", xd
+
+    xdf = np.digitize(x, xBins, right=True)
+    print "x (right-True) digitized: ", xdf
+
+    dataBinsLn = np.zeros(N-1)
+    for i in range(N-1):
+        dataBinsLn[i] = len(x[xd == i+1])
+
+    dataBins = {}
+    for i in range(N-1):
+        b = x[xd == i+1]
+        dataBins[i] = b
+
+
+    print "dataBinsLn: ", dataBinsLn  
+
+
+    # print "dataBins:", dataBins
+
+    keys = sorted(dataBins.keys())
+    for i, k in enumerate(keys):
+        v = dataBins[k]
+        print "xBin[i]: ", i, xBins[i], xBins[i+1]
+        print "mean: ", np.mean(v)
+
+# N=20, 6 secs
+# N=30, 14 secs
+# N=50, 43 secs
+# N=100, 160 secs
+# N=256, 16 minutes
+def digitizeWindow(x, y, z, N):
+
+    print "len data: ", len(x), len(y), len(z)
+
+    assert len(x) == len(y)
+    assert len(y) == len(z)
+
+    # x = x[:100]
+    # y = y[:100]
+
+    # figure out how to group our data
+    xMin = np.min(x)
+    xMax = np.max(x)
+    yMin = np.min(y)
+    yMax = np.max(y)
+
+    # what's the size of each cell?
+    xBins = np.linspace(xMin, xMax, N)
+    yBins = np.linspace(yMin, yMax, N)
+
+    #print "bins:"
+    #print xBins
+    #print yBins
+
+    
+    xd = np.digitize(x, xBins)
+    yd = np.digitize(y, yBins)
+
+    #print "xd", xd
+    #print "yd", yd
+
+
+    xt = yt = 0
+    bins = {}
+
+    # lbins = np.zeros((N, N))
+    lbins = np.zeros((N-1, N-1))
+    total = 0
+    # for xi in range(1, N+1):
+    for xi in range(N-1):
+        print xi
+        # for yi in range(1, N+1):
+        for yi in range(N-1):
+            #print xi, yi
+            # import ipdb; ipdb.set_trace()
+            #xij = [x[i] for i in range(len(x)) if xd[i] == xi and yd[i] == yi]
+            # yij = [y[i] for i in yd if i == yi]
+            #yij = [y[i] for i in range(len(y)) if xd[i] == xi and yd[i] == yi]
+            #bins[(xi, yi)] = (xij, yij)
+            #assert len(xij) == len(yij)
+            #lbins[xi-1][yi-1] = len(xij)
+            xx = xd == xi + 1
+            yy = yd == yi + 1
+            thisBin = np.logical_and(xx, yy)
+            # t = len(xd[np.logical_and(xx, yy)])
+            xThisBin = x[thisBin]
+
+            assert np.all(np.logical_and(xThisBin >= xBins[xi], xThisBin < xBins[xi+1]))
+            # lenXx2 = xThisBin[np.logical_and(xThisBin >= xBins[xi], xThisBin < xBins[xi+1])]
+
+            print "passed assertion in x for ", xi, xBins[xi], xBins[xi+1]
+            print "mean in this bin: ", np.mean(xThisBin)
+            if len(xThisBin) > 0:
+                print "mean in this bin: ", np.min(xThisBin), np.max(xThisBin),np.mean(xThisBin)
+
+            yThisBin = y[thisBin]
+            zThisBin = z[thisBin]
+            bins[(xi, yi)] = (xThisBin, yThisBin, zThisBin)
+            t = len(xd[thisBin])
+            lbins[xi][yi] = t
+            total += t
+            # error check
+            #xt += len(xij)
+            #yt += len(yij)
+
+    print lbins
+    print "total: ", total
+#    print "total bin len: ", xt, yt
+
+
+    # for k, v in bins.items():
+    keys = sorted(bins.keys())
+    for k in keys:
+        v = bins[k]
+        print ""
+        print k
+        xi, yi = k
+        xij, yij, zij = v
+        # print "for x, y bins:", xBins[xi], yBins[yi]
+        # if xi < N:
+        #     print "for x bins:", xBins[xi], xBins[xi+1]
+        #     print "for y bins:", yBins[yi], yBins[yi+1]
+        # else:
+        #     print "for x, y bins:", xBins[xi], yBins[yi]
+        #     print "for x, y bins:", xBins[xi], yBins[yi]
+        print "for x bins:", xBins[xi], xBins[xi+1]
+        print "for y bins:", yBins[yi], yBins[yi+1]
+
+        print "lengths: ", len(xij), len(yij), len(zij)
+        print "means: ", np.mean(xij), np.mean(yij), np.mean(zij)
+
+    return xBins, yBins, bins, lbins
+
+def testDigitizeWindow():
+    print "loading dish data ..."
+    x, y, z = getDishXYZ()
+    print "starting digitization ..."
+
+    # x = np.array(range(3)) + 0.5
+    # y = np.array(range(3)) + 0.5
+    # xm, ym = np.meshgrid(x, y)
+    # print x, y
+    # print xm, ym
+    # z = xm * ym
+
+    # digitizeWindow(xm.flatten(), ym.flatten(), z.flatten(), 3)
+    #digitizeWindow(x, y, z, 10)
+    xd, yd, bins, lbins = digitizeWinDask(x, y, z, 3)
+
+def demoDigitizeWindowScan(file=None, N=None):
+    "Demonstrates that we can use digitize to take means and represent the dish"
+    
+    if N is None:
+        N = 30
+
+    if file is None:
+        file = "Scan-9-sample.ptx.csv"
+
+        
+    x, y, z = getDishXYZ(fpath = "Scan-9-sample.ptx.csv")
+    xd, yd, bins, lbins = digitizeWindow(x, y, z, N)
+
+    imagePlot(lbins, "point density")
+
+    # create the x y we need for 3d plots using our bins
+    xm, ym = np.meshgrid(xd[:-1], yd[:-1], indexing='ij')
+
+    scatter3dPlot(xm, ym, lbins, "point density")
+
+    # calculate the means of each bin
+    zm = np.zeros((N-1, N-1))
+    for i in range(N-1):
+        for j in range(N-1):
+            xs, ys, zs = bins[(i,j)]
+            zm[i][j] = np.mean(zs)
+
+    imagePlot(zm, "means of z")
+
+    scatter3dPlot(xm, ym, zm, "means of z")
+
+    scatter3dPlot(x, y, z, "original data", sample=0.1)
+
+def selfWindow(x, y, z, N):
+
+    assert len(x) == len(y)
+    assert len(y) == len(z)
+
+    # figure out how to group our data
+    xMin = np.min(x)
+    xMax = np.max(x)
+    yMin = np.min(y)
+    yMax = np.max(y)
+
+    # what's the size of each cell?
+    xRange = np.linspace(xMin, xMax, N)
+    yRange = np.linspace(yMin, yMax, N)
+
+    xSize = (xMax - xMin) / N
+    ySize = (yMax - yMin) / N
+
+    print "x, y sizes: ", xSize, ySize
+
+    r = np.sqrt(xSize**2 + ySize**2) / 2.
+
+    print "radius of each group", r
+
+    # here's our collection nodes
+    rs = {}
+
+    # mark the first one
+    rs[(x[0], y[0])] = [(x[0], y[0], z[0])]
+
+    for i in range(1, len(x)):
+        # import ipdb; ipdb.set_trace()
+        xi = x[i]
+        yi = y[i]
+        zi = z[i]
+
+        # find distance between all nodes
+        inBin = False
+        for xn, yn in rs.keys():
+            rn = np.sqrt((xn - xi)**2 + (yn - yi)**2)
+            if rn <= r:
+                rs[(xn, yn)].append((xi, yi, zi))
+                inBin = True
+                break
+
+        # if we aren't in a bin, make a new one
+        if not inBin:
+            rs[(xi, yi)] = [(xi, yi, zi)]
+
+
+    # rss = np.zeros((N, N))
+    t = 0
+    for k, v in rs.items():
+        print k, len(v)
+        t += len(v)
+        # rss[int(k[0]), int(k[1])] = len(v)
+        
+    print "total: ", t
+    
+    rss = makeDistImg(rs, N)    
+    imagePlot(rss, "rss")
+        
+    keys = rs.keys() 
+    xns = [x for x, y in keys]   
+    yns = [y for x, y in keys]
+    zns = [len(v) for k, v in rs.items()]   
+    scatterPlot(xns, yns, "xns, yns")
+    scatter3dPlot(xns, yns, zns, "xns, yns, zns")
+
+    return rs
+
+def makeDistImg(rs, N):
+
+    xs = [x for x, y in rs.keys()]
+    xmax = np.max(xs)
+    xmin = np.min(xs)
+
+    ys = [y for x, y in rs.keys()]
+    ymax = np.max(ys)
+    ymin = np.min(ys)
+
+    rss = np.zeros((N, N))
+    for k, v in rs.items():
+        x, y = k
+        xn = int((x - xmin) / N)
+        yn = int((y - ymin) / N)
+        rss[xn, yn] = len(v)
+
+    return rss
+
+def getRegularXY(m):
+    xs = np.array(range(m), dtype=float)
+    ys = np.array(range(m), dtype=float)
+
+    xm, ym = np.meshgrid(xs, ys)
+    return xm, ym
+
+def getRandomXY(m, rng):
+
+    xs = np.random.random(m) * rng
+    ys = np.random.random(m) * rng
+
+    print "random xs:", xs
+    print "random ys:", ys
+    xm, ym = np.meshgrid(xs, ys)
+
+    return xm, ym
+
+def getLeicaXYZ():
+
+    path = "/home/sandboxes/jbrandt/Telescope27Mar2019"
+    f = "Scan-9_5100x5028_20190327_1145_ReIoNo_ReMxNo_ColorNo_.ptx"
+    fpath = os.path.join(path, f)
+
+    with open(fpath, 'r') as f:
+        ls = f.readlines()
+
+    x, y, z, i = getRawXYZ(ls)    
+
+    return x, y, z
+
+def createSamplePTXFile():
+
+    path = "/home/sandboxes/jbrandt/Telescope27Mar2019"
+    f = "Scan-9_5100x5028_20190327_1145_ReIoNo_ReMxNo_ColorNo_.ptx.csv"
+    fpath = os.path.join(path, f)
+
+    with open(fpath, 'r') as f:
+        ls = f.readlines()
+
+    print "opened original file"
+    dataLs = ls
+
+    # fSample = "Sample-%s" % f
+    fSample = "Scan-9-sample.ptx.csv"
+
+    prcnt = 10.
+    numSamples = int(len(dataLs) / prcnt)
+    # idx = random.sample(range(len(dataLs)), numSamples)
+
+    print "getting sample lines"
+    # sampleLs = [l for i, l in enumerate(dataLs) if i in idx]
+    sampleLs = random.sample(dataLs, numSamples)
+
+    print "writing sample file"
+    with open(fSample, 'w') as f:
+        f.writelines(sampleLs)
+
+    
+def getDishXYZ(fpath=None):
+
+    if fpath is None:
+        path = "/home/sandboxes/jbrandt/Telescope27Mar2019"
+        f = "Scan-9_5100x5028_20190327_1145_ReIoNo_ReMxNo_ColorNo_.ptx.csv"
+        fpath = os.path.join(path, f)
+
+    return importCsv(fpath)
+
+    
+def selfWindowTest():
+
+
+    m = 100
+    # getRegularXY(m)
+    # xm, ym = getRandomXY(m, m)
+    
+    # zs = xm * ym
+    xm, ym, zs = getDishXYZ()
+    xm, ym, zs = sampleXYZData(xm, ym, zs, 1.0)
+    print "num points: ", len(xm)
+    
+    # visualize our test input
+    scatterPlot(xm.flatten(), ym.flatten(), "org data", sample=10.0)
+
+    rs = selfWindow(xm.flatten(), ym.flatten(), zs.flatten(), m)
 
 def windowData(x, y, z, N):
 
@@ -256,4 +739,7 @@ def main():
     print(zWinSmooth)
 
 if __name__=='__main__':
-    main()
+    # selfWindowTest()
+    # testDigitizeWindow()
+    demoDigitize()
+    # createSamplePTXFile()    
