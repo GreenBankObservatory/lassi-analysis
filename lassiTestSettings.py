18830c0bd522754b6d64a01872e20140141d957c
Paul Marganian <pmargani@nrao.edu>
2019-11-15 11:41:15 -0500

    added paths to data in lassiTestSettings.py

--- lassiTestSettings.py
+++ lassiTestSettings.py
@@ -6,6 +6,8 @@ SETTINGS_27MARCH2019 = {
     'xOffset': -8.0,
     'yOffset': 50.0,
     'rot': -10.0,
+    'radius': 47.0,
+    'dataPath': DATA_27MARCH2019
     }
 """
 These are the settings used to extract the scans during development.
@@ -17,24 +19,30 @@ SETTINGS_12JUNE2019 = {
     'xOffset': -8.0,
     'yOffset': 50.0,
     'rot': -10.0,
-    'radius': 47.0
+    'radius': 47.0,
+    'dataPath': '/home/scratch/pmargani/LASSI/scannerData/'
     }
 SETTINGS_17SEPTEMBER2019 = {
     'xOffset': -45.0,
     'yOffset': -5.0,
     'rot': 80.0,
-    'radius': 45.5
+    'radius': 45.5,
+    'dataPath': '/home/sandboxes/pmargani/LASSI/data/17sep2019'
     }
 SETTINGS_10OCTOBER2019 = {
     'xOffset': -52.0,
     'yOffset': -8.0,
     'rot': 80.0,
     'radius': 49.0,
+    'dataPath': '/home/sandboxes/pmargani/LASSI/data/9oct2019'
+
     }
 SETTINGS_11OCTOBER2019 = {
     'xOffset': -44.0,
     'yOffset': -6.5,
     'rot': 80.0,
     'radius': 49.0,
+    'dataPath': '/home/sandboxes/pmargani/LASSI/data/9oct2019'
     }
+
 DATA_UNIT_TESTS = "/home/scratch/pmargani/LASSI/unitTestData/"
