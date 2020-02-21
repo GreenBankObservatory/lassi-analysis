5f0c9be033e15716b545eba466f29ba56cb9eeab
Pedro Salas <psalas@nrao.edu>
2020-02-20 19:57:02 -0500

    Updated unit tests

--- tests/TestLassiAnalysis.py
+++ tests/TestLassiAnalysis.py
@@ -5,12 +5,13 @@ import numpy as np
 
 from plotting import scatter3dPlot
 
-from lassiAnalysis import regridXYZ, regridXYZMasked, maskLeicaData
+from lassiAnalysis import maskLeicaData
 from lassiAnalysis import extractZernikesLeicaScanPair
 import lassiTestSettings
 
 class TestLassiAnalysis(unittest.TestCase):
     "Test methods in lassiAnalysis that don't call gpu smoothing"
+
 
     def setUp(self):
 
@@ -21,59 +22,6 @@ class TestLassiAnalysis(unittest.TestCase):
         # unittest.main(argv=[''], verbosity=2, exit=False)
         self.plots = False
 
-    def testRegridXYZ(self):
-        "Make sure resampling happens sensibly"
-
-        # construct some cartesian data
-        M = 100
-        width = 10.
-        maxMin = width/2.
-        x = np.linspace(-maxMin, maxMin, M)
-        y = np.linspace(-maxMin, maxMin, M)
-        xm, ym = np.meshgrid(x, y)
-        z = xm * ym
-
-        if self.plots:
-            scatter3dPlot(xm, ym, z, "org")
-
-        N = 10
-        x2, y2, z2 = regridXYZ(xm, ym, z, n=N)        
-
-        self.assertEquals(x2.shape, (N, N))
-        self.assertEquals(y2.shape, (N, N))
-        self.assertEquals(z2.shape, (N, N))
-
-        # check the x, y grid - based of min and maxs
-        self.assertEquals(x2[0,0], xm[0,0])
-        self.assertEquals(y2[0,0], ym[0,0])
-
-        # spot check the regridded data
-        self.assertEquals(z[0, 0], 25.0)
-
-        # make sure the residuals are low
-        for i in range(N):
-            for j in range(N):
-                # print i, j, x2[i, j], y2[i, j], np.abs((x2[i,j] * y2[i,j]) - z2[i, j])
-                diff = np.abs((x2[i,j] * y2[i,j]) - z2[i, j])
-                self.assertTrue(diff < 1e-2)
-
-        if self.plots:
-            scatter3dPlot(x2, y2, z2, "regrid")
-
-        # now, how do these max values work?
-        xmin = ymin = -1.0
-        x2, y2, z2 = regridXYZ(xm, ym, z, n=N, xmin=xmin, ymin=ymin)        
-
-        if self.plots:
-            scatter3dPlot(x2, y2, z2, "regrid")
-
-        self.assertEquals(x2.shape, (N, N))
-        self.assertEquals(y2.shape, (N, N))
-        self.assertEquals(z2.shape, (N, N))
-
-        # check the x, y grid 
-        self.assertEquals(x2[0,0], xmin)
-        self.assertEquals(y2[0,0], ymin)
 
     def testMaskLeicaData(self):
         "Test that we can mask out bumps in a smoothed data"
