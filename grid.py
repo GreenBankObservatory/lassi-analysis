bbdc8fae733656f6278758be3952dfdecd3d0be6
Pedro Salas <psalas@nrao.edu>
2020-05-24 19:11:49 -0400

    Added docstring to methods of grid.py.

--- grid.py
+++ grid.py
@@ -9,13 +9,41 @@ from scipy.interpolate import griddata
 
 def regridXYZ(x, y, z, n=512., verbose=False, xmin=False, xmax=False, ymin=False, ymax=False, method='linear'):
     """
-    Regrids the XYZ data to a regularly sampled cartesian grid.
+    Regrids a scan to a regularly sampled square cartesian grid.
+    
+    Parameters
+    ----------
+    x : array
+        Array with the x coordinates.
+    y : array
+        Array with the y coordinates.
+    z : array
+        Array with the z coordinates.
+    n : int, optional
+        Number of pixels per side of the grid.
+        The grid will have `n`x`n` pixels.
+    verbose : bool, optional
+        Verbose output?
+    xmin : float, optional
+        Minimum x value of the grid. 
+        Defaults to the minimum valid value of `x`.
+    xmax : float, optional
+        Maximum x value of the grid. 
+        Defaults to the maximum valid value of `x`.
+    ymin : float, optional
+        Minimum y value of the grid. 
+        Defaults to the minimum valid value of `y`.
+    ymax : float, optional
+        Maximum y value of the grid. 
+        Defaults to the maximum valid value of `y`.
+    method : {‘linear’, ‘nearest’, ‘cubic’}, optional
+        See `scipy.interpolate.griddata` for details.
 
-    :param x: vector with the x coordinates.
-    :param y: vector with the y coordinates.
-    :param z: vector with the z coordinates.
-    :param n: number of samples in the grid.
-    :param verbose: verbose output?
+    Returns
+    -------
+    tuple
+        Tuple with the x coordinates of the grid, 
+        the y coordinates and the z values.
     """
 
     # Set the grid limits.
@@ -53,8 +81,45 @@ def regridXYZ(x, y, z, n=512., verbose=False, xmin=False, xmax=False, ymin=False
     return grid_xy[0], grid_xy[1], reg_z
 
 
-def regridXYZMasked(x, y, z, n=512, verbose=False, xmin=False, xmax=False, ymin=False, ymax=False):
+def regridXYZMasked(x, y, z, n=512, verbose=False, xmin=False, xmax=False, ymin=False, ymax=False, method='linear'):
     """
+    Regrids a scan to a regularly sampled square cartesian grid.
+    It also regrid the mask to the new grid.
+    It calls `regridXYZ` twice, once for the data, and once for the mask.
+    
+    Parameters
+    ----------
+    x : array
+        Array with the x coordinates.
+    y : array
+        Array with the y coordinates.
+    z : array
+        Array with the z coordinates.
+    n : int, optional
+        Number of pixels per side of the grid.
+        The grid will have `n`x`n` pixels.
+    verbose : bool, optional
+        Verbose output?
+    xmin : float, optional
+        Minimum x value of the grid. 
+        Defaults to the minimum valid value of `x`.
+    xmax : float, optional
+        Maximum x value of the grid. 
+        Defaults to the maximum valid value of `x`.
+    ymin : float, optional
+        Minimum y value of the grid. 
+        Defaults to the minimum valid value of `y`.
+    ymax : float, optional
+        Maximum y value of the grid. 
+        Defaults to the maximum valid value of `y`.
+    method : {‘linear’, ‘nearest’, ‘cubic’}, optional
+        See `scipy.interpolate.griddata` for details.
+
+    Returns
+    -------
+    tuple
+        Tuple with the x coordinates of the grid, 
+        the y coordinates and the z values.
     """
 
     outMask = np.ma.masked_invalid(x).mask
@@ -63,13 +128,15 @@ def regridXYZMasked(x, y, z, n=512, verbose=False, xmin=False, xmax=False, ymin=
                                y[~outMask],
                                z[~outMask],
                                n=n, verbose=verbose,
-                               xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax)
+                               xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
+                               method=method)
 
     _,_,retroMask = regridXYZ(x[~outMask],
                               y[~outMask],
                               z.mask.astype(float)[~outMask],
                               n=n, verbose=verbose,
-                              xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax)
+                              xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
+                              method=method)
 
     zRegMasked = np.ma.masked_where(retroMask, zReg)
 
