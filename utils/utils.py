cfd2597de801a4bbc6e894d971a016edbf2ff8b1
Pedro Salas <psalas@nrao.edu>
2020-05-24 20:00:39 -0400

    Added docstrings to methods of utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -9,64 +9,73 @@ from numpy.lib.stride_tricks import as_strided
 from skimage import morphology
 
 from astropy.time import Time
-from astropy.coordinates import cartesian_to_spherical
-from astropy.coordinates import spherical_to_cartesian
+#from astropy.coordinates import cartesian_to_spherical
+#from astropy.coordinates import spherical_to_cartesian
 
 
 def mjd2utc(mjd):
-    "Converts MJD values to UTC datetime objects"
+    """
+    Converts MJD values to UTC datetime objects
+    """
+
     t = Time(mjd, format='mjd')
     t.format = 'datetime'
     return t.value
 
 
 def utc2mjd(dt):
-    "Converts MJD values to UTC datetime objects"
+    """
+    Converts UTC values to MJD datetime objects
+    """
+
     t = Time(dt, format='datetime')
     t.format = 'mjd'
     return t.value
 
 
-def cart2sph(x, y, z, verbose=True):
-    "Wrapper around astropy's cartesian_to_spherical"
-    rs, lats, lngs = cartesian_to_spherical(x, y, z)
-
-    # convert from astropy Quantities to simple numpy
-    rs = np.array([r.value for r in rs])
-    lats = np.array([l.value for l in lats])
-    lngs = np.array([l.value for l in lngs])
-
-    if verbose:
-        print("min/max lats (radians)", lats.min(), lats.max())
-        print("lats supposed range (radians)", -np.pi/2, np.pi/2)
-        print("min/max lngs (radians)", lngs.min(), lngs.max())
-        print("lngs supposed range (radians)", 0, 2*np.pi)
-
-    return rs, lats, lngs
-
-
-def sph2cart(az, el, r, verbose=True):
-    "Wrapper around astropy's spherical_to_cartesian"
-
-    if verbose:
-        print("min/max az (radians)", np.nanmin(az), np.nanmax(az))
-        print("az supposed range (radians)", -np.pi/2, np.pi/2)
-        print("min/max el (radians)", np.nanmin(el), np.nanmax(el))
-        print("el supposed range (radians)", 0, 2*np.pi)
-        print("radial ranges", np.nanmin(r), np.nanmax(r))
-
-    xs, ys, zs = spherical_to_cartesian(r, az, el)
-
-    # convert from astropy Quantities to simple numpy
-    xs = np.array([x.value for x in xs])
-    ys = np.array([y.value for y in ys])
-    zs = np.array([z.value for z in zs])
-
-    return xs, ys, zs 
+#def cart2sph(x, y, z, verbose=True):
+#    "Wrapper around astropy's cartesian_to_spherical"
+#    rs, lats, lngs = cartesian_to_spherical(x, y, z)
+#
+#    # convert from astropy Quantities to simple numpy
+#    rs = np.array([r.value for r in rs])
+#    lats = np.array([l.value for l in lats])
+#    lngs = np.array([l.value for l in lngs])
+#
+#    if verbose:
+#        print("min/max lats (radians)", lats.min(), lats.max())
+#        print("lats supposed range (radians)", -np.pi/2, np.pi/2)
+#        print("min/max lngs (radians)", lngs.min(), lngs.max())
+#        print("lngs supposed range (radians)", 0, 2*np.pi)
+#
+#    return rs, lats, lngs
+#
+#
+#def sph2cart(az, el, r, verbose=True):
+#    "Wrapper around astropy's spherical_to_cartesian"
+#
+#    if verbose:
+#        print("min/max az (radians)", np.nanmin(az), np.nanmax(az))
+#        print("az supposed range (radians)", -np.pi/2, np.pi/2)
+#        print("min/max el (radians)", np.nanmin(el), np.nanmax(el))
+#        print("el supposed range (radians)", 0, 2*np.pi)
+#        print("radial ranges", np.nanmin(r), np.nanmax(r))
+#
+#    xs, ys, zs = spherical_to_cartesian(r, az, el)
+#
+#    # convert from astropy Quantities to simple numpy
+#    xs = np.array([x.value for x in xs])
+#    ys = np.array([y.value for y in ys])
+#    zs = np.array([z.value for z in zs])
+#
+#    return xs, ys, zs 
 
 
 def aggregateXYZ(x, y, z):
-    "x, y, z -> [(x0, y0, z0), (x1, y1, z1), ...]"
+    """
+    x, y, z -> [(x0, y0, z0), (x1, y1, z1), ...]
+    """
+    
     xyz = []
     for i in range(len(x)):
         xyz.append((x[i], y[i], z[i]))
@@ -74,7 +83,9 @@ def aggregateXYZ(x, y, z):
 
 
 def splitXYZ(xyz):
-    "[(x0, y0, z0), (x1, y1, z1), ...] -> x, y, z"
+    """
+    [(x0, y0, z0), (x1, y1, z1), ...] -> x, y, z
+    """
 
     x = []
     y = []
@@ -89,24 +100,9 @@ def splitXYZ(xyz):
     return x, y, z
 
 
-def log(x):
-    "short cut to numpy log"
-    return np.log(np.abs(x))
-
-
-def difflog(x):
-    "short cut to tnumpy log and diff"
-    return np.log(np.abs(np.diff(x)))
-
-
-def log10(x):
-    "short cut to numpy log10"
-    return np.log10(np.abs(x))
-
-
 def maskDiff(diff, window=(20,20), threshold=2):
     """
-    Generates mask for diff based on the Z scores computed using a rolling window.
+    Generates mask for `diff` based on the Z scores computed using a rolling window.
     """
 
     diff = np.ma.masked_invalid(diff)
@@ -146,6 +142,25 @@ def midPoint(x):
 
 def radialMask(x, y, r, xc=None, yc=None):
     """
+    Generates a circular mask.
+
+    Parameters
+    ----------
+    x : ndarray
+        Array with x coordinates to define a circle.
+    y : ndarray
+        Array with y coordinates to define a circle.
+    r : float
+        Radius of the circular mask. Values <=`r` will be masked.
+    xc : float, optional
+        Center of the circle in the x coordinate.
+    yc : float, optional
+        Center of the circle in the y coordinate.
+
+    Returns
+    -------
+    array of bools
+        Array of the same shape as x and y with True for values inside the circle.
     """
 
     if xc is None:
@@ -158,6 +173,29 @@ def radialMask(x, y, r, xc=None, yc=None):
 
 def ellipticalMask(x, y, xc, yc, bMaj, bMin, angle):
     """
+    Generates an elliptical mask.
+
+    Parameters
+    ----------
+    x : ndarray
+        Array with x coordinates to define an ellipse.
+    y : ndarray
+        Array with y coordinates to define an ellipse.
+    xc : float
+        Center of the ellipse in the x coordinate.
+    yc : float
+        Center of the ellipse in the y coordinate.
+    bMaj : float
+        Major axis of the ellipse.
+    bMin : float
+        Minor axis of the ellipse.
+    angle : float in degrees
+        Rotation angle of the ellipse.
+    
+     Returns
+    -------
+    array of bools
+        Array of the same shape as x and y with True for values inside the ellipse.
     """
 
     cos_angle = np.cos(np.radians(180. - angle))
@@ -256,17 +294,23 @@ def makeGrid(xmin, xmax, dx, ymin, ymax, dy):
 
 
 def _check(a, r_c, subok=False):
-    """Performs the array checks necessary for stride and block.
-    : a   - Array or list.
-    : r_c - tuple/list/array of rows x cols.
-    : subok - from numpy 1.12 added, keep for now
-    :Returns:
-    :------
-    :Attempts will be made to ...
-    :  produce a shape at least (1*c).  For a scalar, the
-    :  minimum shape will be (1*r) for 1D array or (1*c) for 2D
-    :  array if r<c.  Be aware
     """
+    Performs the array checks necessary for stride and block.
+
+    Parameters
+    ----------
+    a : array or list.
+    r_c : tuple/list/array of rows x cols.
+    subok : from numpy 1.12 added, keep for now
+
+    Returns
+    -------
+    tuple
+        Attempts will be made to produce a shape at least (1*c).  
+        For a scalar, the minimum shape will be (1*r) for 1D array 
+        or (1*c) for 2D array if r<c.
+    """
+
     if isinstance(r_c, (int, float)):
         r_c = (1, int(r_c))
     r, c = r_c
@@ -278,19 +322,12 @@ def _check(a, r_c, subok=False):
 
 
 def stride(a, r_c=(3, 3)):
-    """Provide a 2D sliding/moving view of an array.
-    :  There is no edge correction for outputs.
-    :
-    :Requires:
-    :--------
-    : _check(a, r_c) ... Runs the checks on the inputs.
-    : a - array or list, usually a 2D array.  Assumes rows is >=1,
-    :     it is corrected as is the number of columns.
-    : r_c - tuple/list/array of rows x cols.  Attempts  to
-    :     produce a shape at least (1*c).  For a scalar, the
-    :     minimum shape will be (1*r) for 1D array or 2D
-    :     array if r<c.  Be aware
     """
+    Provide a 2D sliding/moving view of an array.
+    There is no edge correction for outputs.
+    
+    """
+    
     a, r, c, r_c = _check(a, r_c)
     shape = (a.shape[0] - r + 1, a.shape[1] - c + 1) + r_c
     strides = a.strides * 2
@@ -302,9 +339,16 @@ def rolling_stat(a, func=np.nanstd, **kwargs):
     """
     Statistics on the last two dimensions of an array.
 
-    :param a: Array where to compute the statistics.
-    :param func: Function used to compute the statistics, e.g., np.nanmean.
+    Parameters
+    ----------
+    a : 2-D array 
+        Array where to compute the statistics.
+    func : callable, optinal
+        Function used to compute the statistics, e.g., np.nanmean.
+    kwargs : optinal
+        Keyword arguments passed to `func`.
     """
+
     a = np.asarray(a)
     a = np.atleast_2d(a)
     ax = None
@@ -319,10 +363,17 @@ def rolling_stat(a, func=np.nanstd, **kwargs):
 def getRollingStat(z, func=np.nanstd, window=(4,4), **kwargs):
     """
     Computes statistics over a 2D array using a rolling window.
+    It will pad the array during the computation to fit the rolling window
+    on the array's edges.
 
-    :param z: 2D array.
-    :param func: Function used to compute the statistics, e.g., np.nanmean.
-    :param window: Rolling window size.
+    Parameters
+    ----------
+    z : 2-D array
+        Array where to compute the statistics.
+    func : callable
+        Function used to compute the statistics, e.g., np.nanmean.
+    window : tuple of ints, optional
+        Rolling window size.
     """
     
     zs = z.shape
@@ -340,9 +391,14 @@ def padArray(a, target_shape, fill_value=np.nan):
     """
     Pads a 2D array with a fill value to match the target shape.
 
-    :param a: 2D array to pad.
-    :param target_shape: Shape of the padded array.
-    :param fill_value: Value to use on the new rows and columns.
+    Parameters
+    ----------
+    a : 2-D array 
+        Array to pad.
+    target_shape : tuple
+        Shape of the padded array.
+    fill_value : float, optional
+        Value to use on the new rows and columns.
     """
 
     a_shape = a.shape
