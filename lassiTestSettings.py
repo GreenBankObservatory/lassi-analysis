b551ca132c5cdad5b60e0767b939260df2c2d8d9
Pedro Salas <psalas@nrao.edu>
2020-06-02 11:31:18 -0400

    Switched test files back to the March 27, 2019 scans.

--- lassiTestSettings.py
+++ lassiTestSettings.py
@@ -3,6 +3,7 @@ import pickle
 DATA_27MARCH2019 = "/home/sandboxes/jbrandt/Telescope27Mar2019"
 SCAN9 = "Scan-9_5100x5028_20190327_1145_ReIoNo_ReMxNo_ColorNo_.ptx"
 SCAN11 = "Scan-11_5100x5028_20190327_1155_ReIoNo_ReMxNo_ColorNo_.ptx"
+
 SETTINGS_27MARCH2019 = {
     'xOffset': -8.0,
     'yOffset': 50.0,
@@ -70,8 +71,6 @@ SETTINGS_15MAR2020 = {
     'dataPath': '/home/gbtdata/TLASSI_200315'
     }
 
-
-# DATA_UNIT_TESTS = "/home/scratch/pmargani/LASSI/unitTestData/"
 
 def getData(settings):
 
